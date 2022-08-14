c18cc09314d4ede1ae5049d34cf9129550b7282f
dalihabli <dali.freerunner@gmail.com>
2022-08-14 09:43:14 +0100

    hooks

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import {useState,useEffect} from 'react';
+import './App.css';
+import MovieCard from './components/MovieCard/MovieCard';
+import MovieList from './components/MovieList/MovieList';
+import Filtring from './components/filtring/Filtring';
+import AddMovie from './components/AddMovie/AddMovie';
+
+
+const info =[
+  {title:'titanic' , img:'/image/titanic.jpg', description:"romontic", posterURL:"www.titanic.com", rating:9.4},
+  { title:'mission impossible', img:'/image/mission impossible.jpg', description:"Action", posterURL:"www.missionimpossible.com", rating:9.3 },
+  { title:'uncharted', img:'/image/uncharted.jpg', description:"aventure", posterURL:"www.uncharted.com", rating:8.5 },
+]
+
+
+
+function App() {
+return <div className="App">
+  
+  
+  
+  
+  
+</div>;
+
+}
+
+export default App;
